3e6ed5a40191f0e027cb9175d75a9b1e136d40c0
Gregory Gerard <gregory@flatchr.io>
2024-01-07 00:08:23 +0100

    feat: add quizz questions

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -1,12 +1,26 @@
 import { useState } from "react"
+import QUESTIONS from '../questions';
 
-export default function Quiz() {
-    const [activeQuestionIndex, setActiveQuestionIndex] = useState(0);
-    
+export default function Quiz() {  
     const [userAnswers, setUserAnswers] = useState([]);
+    const activeQuestionIndex = userAnswers.length;
+
+    function handleSelectedAnswer(selectedAnswer) {
+        setUserAnswers(prevState => [...prevState, selectedAnswer]);
+    }
+
     return (
-        <>
-            <p>Currently Active Question</p>
-        </>
+        <div id="quiz">
+            <div id="question">
+                <h2>{QUESTIONS[activeQuestionIndex].text}</h2>
+                <ul id="answers">
+                    {QUESTIONS[activeQuestionIndex].answers.map(answer => (
+                    <li key={answer} className="answer">
+                        <button onClick={() => handleSelectedAnswer(answer)}>{answer}</button>
+                    </li>
+                    ))}
+                </ul>
+            </div>
+        </div>
     );
 }
