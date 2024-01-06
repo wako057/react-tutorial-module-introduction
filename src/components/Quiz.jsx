45ed2438e87c00200bec75594f12d077bc4c4372
Gregory Gerard <gregory@flatchr.io>
2024-01-07 00:19:12 +0100

    feat: avoid error on question end

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -1,20 +1,32 @@
 import { useState } from "react"
 import QUESTIONS from '../questions';
+import quizCompleteImg from '../assets/quiz-complete.png'
 
 export default function Quiz() {  
     const [userAnswers, setUserAnswers] = useState([]);
     const activeQuestionIndex = userAnswers.length;
+    const quizisComplete = QUESTIONS.length === activeQuestionIndex;
 
     function handleSelectedAnswer(selectedAnswer) {
         setUserAnswers(prevState => [...prevState, selectedAnswer]);
     }
+
+    if (quizisComplete) {
+        return (
+            <div id="summary">
+                <img src={quizCompleteImg} alt="Trophy Icon"/>
+                <h2>Quiz Completed!</h2>
+            </div>
+        );
+    }
+    const shuffledAnswer = [ ...QUESTIONS[activeQuestionIndex].answers ].sort(() => Math.random() - 0.5);
 
     return (
         <div id="quiz">
             <div id="question">
                 <h2>{QUESTIONS[activeQuestionIndex].text}</h2>
                 <ul id="answers">
-                    {QUESTIONS[activeQuestionIndex].answers.map(answer => (
+                    {shuffledAnswer.map(answer => (
                     <li key={answer} className="answer">
                         <button onClick={() => handleSelectedAnswer(answer)}>{answer}</button>
                     </li>
